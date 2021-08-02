387dacabe2ec984f385e6bd50fc265439f8d43d6
g0tie <gaultierbegue@outlook.com>
2021-08-02 21:47:19 +0400

    add spyCheckup script

---
+++ amISpied.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+isMicUsed=$(grep owner_pid /proc/asound/card*/pcm*/sub*/status)
+isCamUsed=$(lsof /dev/video*)
+
+
+function killmicrocesses {
+echo "Would you like to kill all those processes ? (y/n) "
+read answer
+
+micPids=($(grep -E owner_pid /proc/asound/card*/pcm*/sub*/status | cut -d":" -f3 | tr -s " "))
+
+if [ "$answer" == "y" || "$answer" == "Y" ]
+then
+	for pid in "${micPids[@]}" 
+	do
+		kill $pid
+	done
+else
+	exit 0
+fi
+}
+
+function killcamprocesses {
+echo "Would you like to kill all those processes ? (y/n) "
+read answer
+
+camPids=($(lsof /dev/video* | cut -d' ' -f3 | uniq | sed '/^[[:space:]]*$/d' ))
+
+if [ "$answer" == "y" || "$answer" == "Y" ]
+then
+	for pid in "${camPids[@]}" 
+	do
+		kill $pid
+	done
+else
+	exit 0
+fi
+}
+
+if [ -z "$isMicUsed" ]
+then
+	echo "Microphone isn't used"
+else
+	echo "Microphone is used"
+	echo "-----------------"
+	echo $isMicUsed
+	killmicrocesses
+fi
+
+if [ -z "$isCamUsed" ]
+then
+	echo "Camera isn't used"
+else
+	echo "Camera is used"
+	echo "-----------------"
+	echo $isCamUsed
+	killcamprocesses
+fi
+
