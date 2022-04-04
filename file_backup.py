74d2ed72ef87cabe7f9088008015599fbea7a37d
g0tie <gaultierbegue@outlook.com>
2022-04-04 16:16:31 +0400

    writing concept of new script

---
+++ file_backup.py
@@ -0,0 +1,4 @@
+# sort $HOME and dont take configs or hidden folders
+# compress them into archive
+# name archive according current date
+# save it on extenal drive or wait until extenal drive is plugged
