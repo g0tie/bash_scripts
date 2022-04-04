d5dc92d8cd642d8ee58dee61baf59abf96ce868b
g0tie <gaultierbegue@outlook.com>
2022-04-04 16:18:30 +0400

    update description

--- file_backup.py
+++ file_backup.py
@@ -1,4 +1,5 @@
 # sort $HOME and dont take configs or hidden folders
 # compress them into archive
 # name archive according current date
+# protect archive with user password 
 # save it on extenal drive or wait until extenal drive is plugged
