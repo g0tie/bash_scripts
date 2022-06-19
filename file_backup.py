14175a2d050429e611272fddac6d5c79830f888b
g0tie <gaultierbegue@outlook.com>
2022-06-19 22:10:09 +0400

    rm unecessary files

--- file_backup.py
+++
@@ -1,5 +0,0 @@
-# sort $HOME and dont take configs or hidden folders
-# compress them into archive
-# name archive according current date
-# protect archive with user password 
-# save it on extenal drive or wait until extenal drive is plugged
--- make_backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-name=$(date +%Y-%b-%m)
-name="backup_$name.tar"
-echo "Creating backup in current folder.."
-tar -cvf $name $HOME/Documents $HOME/Archives $HOME/Images $HOME/Vidéos
-echo "Backup created in $(pwd)"
