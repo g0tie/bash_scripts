cf5e8f342c77138d7afcf90c167d14e909bb837a
g0tie <gaultierbegue@outlook.com>
2022-06-20 12:40:22 +0400

    refactor bash autosort

--- bash_autosort.sh
+++ bash_autosort.sh
@@ -2,34 +2,18 @@
 shopt -s extglob # turn on globbing
 tosort_folder=$1
 
-function Main() {
-
-echo "Starting..."
-pushd $tosort_folder &> /dev/null
-
-#declare associative array
-declare -A extensions 
-
-extensions[doc]=$(ls $(sed 's/\</\*\./g' <<< "pdf odt txt html js css csv sql php xml ini sh docx java class py ai fig drawio cbr ppt json") 2> /dev/null)
-extensions[img]=$(ls $(sed 's/\</\*\./g' <<< "png jpg svg webp jpeg") 2> /dev/null)
-extensions[videos]=$(ls $(sed 's/\</\*\./g' <<< "mp4 avi mov") 2> /dev/null)
-extensions[audio]=$(ls $(sed 's/\</\*\./g' <<< "mp3 ogg wav aac") 2> /dev/null)
-extensions[archives]=$(ls $(sed 's/\</\*\./g' <<< "zip rar tar tar.gz tar.xz deb rpm apk iso tgz jar vsix AppImage") 2> /dev/null)
-
-folders=("Documents" "Images" "Videos" "Audios" "Archives")
-
-#Create appropriate folders if no exists
-mkdir -p ${folders[@]}
-
-moveFiles
-
-popd &> /dev/null
-echo "Files moved"
-ls -lh
-}
-
 function moveFiles() {
 	
+    #declare associative array
+    declare -A extensions 
+
+    extensions[doc]=$(ls $(sed 's/\</\*\./g' <<< "pdf odt txt html js css csv sql php xml ini sh docx java class py ai fig drawio cbr ppt json") 2> /dev/null)
+    extensions[img]=$(ls $(sed 's/\</\*\./g' <<< "png jpg svg webp jpeg") 2> /dev/null)
+    extensions[videos]=$(ls $(sed 's/\</\*\./g' <<< "mp4 avi mov") 2> /dev/null)
+    extensions[audio]=$(ls $(sed 's/\</\*\./g' <<< "mp3 ogg wav aac") 2> /dev/null)
+    extensions[archives]=$(ls $(sed 's/\</\*\./g' <<< "zip rar tar tar.gz tar.xz deb rpm apk iso tgz jar vsix AppImage") 2> /dev/null)
+
+
     for index in "${!extensions[@]}"
     do
 	   case $index in
@@ -55,6 +39,22 @@ function moveFiles() {
 	   fi 
     done
 
+}
+
+function Main() {
+
+    echo "Starting..."
+    pushd $tosort_folder &> /dev/null
+    folders=("Documents" "Images" "Videos" "Audios" "Archives")
+
+    #Create appropriate folders if no exists
+    mkdir -p ${folders[@]}
+
+    moveFiles
+
+    popd &> /dev/null
+    echo "Files moved"
+    ls -lh
 }
 
 Main
