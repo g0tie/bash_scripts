09a2a754d30b6291c059b507989efc6f71322c95
g0tie <gaultierbegue@outlook.com>
2022-06-25 17:10:05 +0400

    add script description and automatically create fodlers in home directory

--- ps_autosort.ps1
+++ ps_autosort.ps1
@@ -1,3 +1,23 @@
+<#
+.SYNOPSIS
+Sort a files from a directory
+
+.DESCRIPTION
+Move files by extensions into the correct folders, if folder doesn't exist it will be create.
+Folders are: Archives, Images, Executables and Documents
+
+.OUTPUTS
+When finished list folder to sort to confirm that files were moved
+
+.EXAMPLE
+PS> ps_autosort.ps1 .\Downloads
+
+.LINK
+Online version: https://github.com/g0tie/scripts
+
+#>
+
+
 [string[]] $folders = "Images", "Archives", "Exectutables", "Documents";
 $folderToSort = $args[0];
 
@@ -6,11 +26,12 @@ if (!$folderToSort) {
 	Exit;
 }
 
+
 function CreateFolders {
 	Set-Location $folderToSort
 
 	"Creating folder {0}..." -F $folder;
-	New-Item -Path $folders -ItemType Directory -Force;		
+	New-Item -Path $HOME\$folders -ItemType Directory -Force;		
 }
 
 function MoveFiles {
@@ -19,7 +40,7 @@ function MoveFiles {
 		images = (Get-ChildItem *.jpg, *.png, *.bmp, *.webp, *.tiff, *.svg).FullName
 		archives = (Get-ChildItem *.zip, *.tar, *.tar.gz, *.rar, *.tgz).FullName
 		executables = (Get-ChildItem *.exe,*.msi).FullName
-		documents =(Get-ChildItem *.pdf, *.doc, *.odt, *.csv, *.txt, *.xls, *xlsx, *.rtf, *.ppt, *.pptx, *.js, *.py, *.php, *.opvpn).FullName
+		documents = (Get-ChildItem *.pdf, *.doc, *.odt, *.csv, *.txt, *.xls, *xlsx, *.rtf, *.ppt, *.pptx, *.js, *.py, *.php, *.opvpn).FullName
 	}
 
 	foreach ($file in $files.GetEnumerator()) { 
